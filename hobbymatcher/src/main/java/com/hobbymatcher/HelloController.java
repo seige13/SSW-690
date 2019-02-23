b14a993614fd1bc2a1a48ebe8ab16b7ae89d6339
bestksl <kslup7@gmail.com>
2019-02-23 17:02:09 -0500

    Version 0.1 ----HaoxuanLi

---
+++ hobbymatcher/src/main/java/com/hobbymatcher/HelloController.java
@@ -0,0 +1,36 @@
+package com.hobbymatcher;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    NamedParameterJdbcTemplate jdbcTemplate;
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World!";
+    }
+
+    @Data
+    static class Result {
+        private int left;
+        private int right;
+        private long answer;
+    }
+
+//    // SQL sample
+//    @RequestMapping("calc")
+//    Result calc(@RequestParam int left, @RequestParam int right) {
+//        MapSqlParameterSource source = new MapSqlParameterSource()
+//                .addValue("left", left)
+//                .addValue("right", right);
+//        return jdbcTemplate.queryForObject("SELECT :left + :right AS answer", source, (rs, rowNum) -> new Result(left, right, rs.getLong("answer")));
+//    }
+}
