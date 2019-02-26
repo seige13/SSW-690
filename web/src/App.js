29d6cefa09269e1e7299b017a0e1627bbe5235ef
Chris B <cboffa13@gmail.com>
2019-02-26 13:53:55 -0800

    Add partial sign up page and Travis CI (#4)
    * Sign up page and Travis CI integration
    
    * Move travis.ci file
    
    Add before script to travis ci
    
    Add install command
    
    Add enyzme for shallow rendering of components

--- web/src/App.js
+++ web/src/App.js
@@ -1,36 +1,58 @@
-import React, { Component } from "react";
-import { Nav, Navbar } from "react-bootstrap";
-import NavLink from "react-bootstrap/NavLink";
-import { LinkContainer } from "react-router-bootstrap";
-import Routes from "./Routes";
-import "./App.css";
-
+import React, { Component, Fragment } from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import NavLink from 'react-bootstrap/NavLink';
+import { LinkContainer } from 'react-router-bootstrap';
+import Routes from './Routes';
+import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  };
+
+  handleLogout = event => {
+    this.userHasAuthenticated(false);
+  };
+
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+
     return (
       <div className="App container">
         <Navbar bg="light" expand="lg">
           <LinkContainer to="/">
-            <Navbar.Brand >Hobby Matcher</Navbar.Brand>
+            <Navbar.Brand>Hobby Matcher</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className={"pullRight"}>
+          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <LinkContainer to="/sign-up">
-                <NavLink>
-                  Sign Up
-                </NavLink>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <NavLink>
-                  Login
-                </NavLink>
-              </LinkContainer>
+              {this.state.isAuthenticated ? (
+                <NavLink onClick={this.handleLogout}>Logout</NavLink>
+              ) : (
+                <Fragment>
+                  <LinkContainer to="/signup">
+                    <NavLink>Signup</NavLink>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavLink>Login</NavLink>
+                  </LinkContainer>
+                </Fragment>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <Routes />
+        <Routes childProps={childProps} />
       </div>
     );
   }
