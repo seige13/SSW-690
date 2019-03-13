c0b64d31ef806fa64ce25547d78a13b48f3759b3
bestksl <kslup7@gmail.com>
2019-03-12 20:03:35 -0400

    Change the architecture from springboot to the Spring mvc spring mybatis. (#23)
    Change the architecture from springboot to the Spring mvc spring mybatis.

---
+++ hobbymatcher/src/main/test/java/com/hobbymatcher/test/service/UserServiceTest.java
@@ -0,0 +1,24 @@
+package com.hobbymatcher.test.service;
+
+import com.hobbymatcher.entity.User;
+import com.hobbymatcher.service.UserService;
+import com.hobbymatcher.test.BaseTest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class UserServiceTest extends BaseTest {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testGetUserList() {
+        List<User> userList = userService.getUserList();
+        assertEquals("robert",  userList.get(1).getNickName());
+    }
+
+}
