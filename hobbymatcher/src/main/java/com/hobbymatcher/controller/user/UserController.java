001e8dbebec70a5a4ae5748e3e94ffe86f8edf2c
Haoxuan Li <kslup7@gmail.com>
2019-04-18 14:34:04 -0400

    fix return msg

--- hobbymatcher/src/main/java/com/hobbymatcher/controller/user/UserController.java
+++ hobbymatcher/src/main/java/com/hobbymatcher/controller/user/UserController.java
@@ -35,10 +35,12 @@ public class UserController {
         try {
             list = userService.getUserList();
             modelMap.put("list", list);
+            modelMap.put("msg", "return list success");
             response.setStatus(200);
         } catch (Exception e) {
             e.printStackTrace();
             modelMap.put("success", false);
+            modelMap.put("msg", e.toString());
             response.setStatus(400);
         }
         return modelMap;
@@ -50,10 +52,14 @@ public class UserController {
     private Map<String, Object> login(@RequestBody User user, HttpServletRequest request, HttpServletResponse response) {
         Map<String, Object> modelMap = new HashMap<String, Object>();
         String passwordByMd5 = Md5.MD5(user.getPassWord());
-        user=userService.findUserByEmail(user.getEmail());
+        user = userService.findUserByEmail(user.getEmail());
         request.getSession().setAttribute("user", user);
         Boolean result = userService.login(user.getEmail(), passwordByMd5);
         modelMap.put("status", result);
+        if (result) {
+            modelMap.put("user", user);
+        }
+        modelMap.put("msg", result ? "login success" : "login failed");
         response.setStatus(result ? 200 : 401);
         return modelMap;
     }
@@ -85,10 +91,12 @@ public class UserController {
             System.out.println(user.getPassWord());
             Boolean result = userService.register(user);
             modelMap.put("status", result);
+            modelMap.put("msg", result ? "add success" : "add failed");
             response.setStatus(result ? 200 : 400);
             return modelMap;
         } else {
             modelMap.put("status", false);
+            modelMap.put("msg", "user object is null, add failed");
             response.setStatus(400);
             return modelMap;
         }
@@ -103,10 +111,12 @@ public class UserController {
         if (user1 != null) {
             Boolean result = userService.deleteUser(user1.getId());
             modelMap.put("status", result);
+            modelMap.put("msg", result ? "delete success" : "delete failed");
             response.setStatus(result ? 200 : 400);
             return modelMap;
         }
         modelMap.put("status", false);
+        modelMap.put("msg", "user object is null, delete failed");
         response.setStatus(400);
         return modelMap;
     }
@@ -119,10 +129,12 @@ public class UserController {
         if (user != null) {
             Boolean result = userService.updateUser(user);
             modelMap.put("status", result);
+            modelMap.put("msg", result ? "update success" : "update failed");
             response.setStatus(result ? 200 : 400);
             return modelMap;
         } else {
             modelMap.put("status", false);
+            modelMap.put("msg", "user object is null, update failed");
             response.setStatus(400);
             return modelMap;
         }
